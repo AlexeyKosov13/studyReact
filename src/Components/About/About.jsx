24305d71df27534c6542ceaedd67c77c72d50607
Alexey <AlexeyKosov13@yandex.ru>
2024-07-09 06:48:32 +0300

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={about_img} alt="" className='about-img' />
+            <img src={play_icon} alt="" className='play-icon' />
+        </div>
+        <div className="about-right">
+            <h3>About university</h3>
+            <h2>Nurturing Tomorrow`s Leaders Today</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos adipisci quo similique doloremque cum, explicabo rem, temporibus molestias quasi, sed repudiandae ipsa excepturi animi. Quae ex id atque odio placeat!
+           </p>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae eius molestiae expedita animi delectus veritatis itaque eveniet quo quam sunt? Minima magni laudantium quae vitae, quos animi nemo perspiciatis veritatis?
+           </p>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae eius molestiae expedita animi delectus veritatis itaque eveniet quo quam sunt? Minima magni laudantium quae vitae, quos animi nemo perspiciatis veritatis?
+           </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "96a57e81-4b6e-4126-848a-2b07f292cd26");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nesciunt porro corporis cupiditate dolores quia obcaecati amet. Nostrum qui maiores quidem consequatur sit enim laboriosam? Laboriosam dolorum voluptate porro quas.</p>
+            <ul>
+                <li><img src={mail_icon} alt="" />Contact@GreatStack.dev</li>
+                <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
+                <li><img src={location_icon} alt="" />Massachusetts Ave, Cambridge<br/>MA 02145, United States</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label >Your name</label>
+                <input type="text" name='name' placeholder='Enter your name' required />
+                <label >Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your phone number' required />
+                <label>Write your messages here</label>
+                <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+                <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
+            </form>
+            <span>{result}</span>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
